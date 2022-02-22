31aabea80446f51022102f4739eb400f6e34d464
Nishant Gupta <reachnishant26@gmail.com>
2022-02-21 20:04:05 -0500

    added new sub-module

---
+++ jpa-entities/src/main/java/usr/nishant/entities/User.java
@@ -0,0 +1,22 @@
+package usr.nishant.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
