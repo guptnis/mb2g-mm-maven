6165c5fe5a1c6ebbe43d77847d99b61faa1ef0e4
Nishant Gupta <reachnishant26@gmail.com>
2022-02-25 17:06:53 -0500

    added flatten-mode plugin

--- web-app/src/main/java/usr/nishant/controllers/UserController.java
+++ web-app/src/main/java/usr/nishant/controllers/UserController.java
@@ -2,12 +2,14 @@ package usr.nishant.controllers;
 
 import usr.nishant.domain.UserCommand;
 import usr.nishant.entities.User;
+import usr.nishant.intellij.HelloFromJB;
 import usr.nishant.mappers.UserMapper;
 
 public class UserController {
 
     User saveUser(UserCommand userCommand) {
 
+        new HelloFromJB().sayHello();
         //adhoc implementation
         return UserMapper.INSTANCE.userCommandToUser(userCommand);
     }
